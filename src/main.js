9822d9e4f3bda3dcaedd9c472e59d1180085ed81
JobbyM <songzhipengmune@gmail.com>
2016-09-02 11:53:06 +0800

    初始化 main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App'
+import Hello from './components/Hello.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+// 注册两个组件
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+
+// 路由map
+router.map({
+	'/hello':{
+		components: Hello
+	}
 })
+
+router.redirect({
+	'*': '/hello'
+})
+
+router.start(App, '#app')
